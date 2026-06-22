28200f5f8bbc7fef3b73c7a5e93f47fbf3c2bd31
ilyaalwot <ghostreaper2765@gmail.com>
2026-06-22 19:02:01 +0300

    .

--- UserIdentifier.java
+++ UserIdentifier.java
@@ -1,6 +1,9 @@
+import java.io.Serializable;
 import java.util.Objects;
 
-public class UserIdentifier {
+public class UserIdentifier implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private int id;
     private String name;
 
